4fca149e40055609d9accb795cbcaab551a33393
Arbeiter617 <ianfischer476@gmail.com>
2021-10-05 18:13:24 -0500

    pushing server project

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -9,17 +9,51 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
 
     // TODO 12: Make the whole thing parallel
-    async.series( 
+    async.parallel( 
         // TODO 9: Supply an array of functions
-        [
-            
-        ],
+        [function one(callback) {
+            wrapper(callback);
+        }, function two(callback) {
+            wrapper(callback);
+        }, function three(callback) {
+            wrapper(callback);
+        }, function four(callback) {
+            wrapper(callback);
+        }],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results); 
 
+            
+
+            for (var index = 0; index < victoryOrder.length; index++) {
+                
+                var variable = victoryOrder[index];
+                res.write(variable + "\n");
+
+                
+
+               
+
+                
+                
+            }
+            let d = new Date();
+            
+            let endTime = d.getTime();
+
+
+            var duration = endTime - startTime;
+
+            res.write(duration + "\n");
+
+            res.end();
+           
         }
     );
     
@@ -27,8 +61,16 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(wrapperTODO,Math.random()*1000);
+    
+    function wrapperTODO() {
+        let d = new Date();
+        callback(null, d.getTime());
+    }
+    
 }
+
+
 
 function sortTogether(names, times) {
     var tempList = [];
--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,40 @@
 // requestServer.js file
+
+var args = process.argv.slice(2);
+
+const http = require("http");
+const request = require("request");
+const port = 8686;
+
+console.log(port);
+
+http.createServer(function(req,res) {
+    var url = args[0] ? args[0] : "coolmathgames.com";
+    request(url, function (error, response, body) {
+   
+        if (!body || !response || (error === null && response.statusCode !== 200)){
+            
+
+            res.end("bad URL\n");
+            return;
+            
+        } else if(response.statusCode === 200 && !error === true) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+
+            res.write(body);
+            res.end();
+        
+
+        } else {
+            res.writeHead(response.statusCode, {'Content-Type': 'text/plain'});
+            res.write(error.toString());
+            res.end();
+        }
+    }
+    
+    
+)}).listen(port);
+
+
+
+
