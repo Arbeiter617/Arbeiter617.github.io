ec5153661f6fc3febcf9a54ff4ba10106cfb3162
Arbeiter617 <ianfischer476@gmail.com>
2021-12-02 17:09:21 -0600

    save message

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -2,6 +2,24 @@ const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
 
-	
+var actuatorRoutes = require('./../routes/actuators');
+app.use('/pi/actuators', actuatorsRoutes);
+
+
+router.route('/').get(function (req, res, next) {
+res.send(resources.pi.actuators);
+});
+
+router.route('/leds').get(function (req, res, next) {
+	res.send(resources.pi.actuators.leds);
+});
+
+router.route('/leds/:1').get(function (req, res, next) {
+	res.send(resources.pi.actuators.leds[req.params.1]);
+});
+
+router.route('/leds/:2').get(function (req, res, next) {
+	res.send(resources.pi.actuators.leds[req.params.2]);
+});
 
 module.exports = router;
