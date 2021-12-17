5f6c70da5063990a24867f3f23ab324b8346acc6
Arbeiter617 <ianfischer476@gmail.com>
2021-12-16 18:13:47 -0600

    save message

--- iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
+++ iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
@@ -1,8 +1,15 @@
 const resources = require('./../../resources/model');
+const Gpio = require('onoff').Gpio;
+
 
 let actuator1, actuator2;
 let model = resources.pi.actuators.leds;
 let pluginName = resources.pi.actuators.leds[1].name + ", " + resources.pi.actuators.leds[2].name;
+
+
+
+
+
 
 exports.start = function (params) {
 	connectHardware();
@@ -10,3 +17,10 @@ exports.start = function (params) {
 };
 
 // TODO 1: Complete the ledsPlugin!
+actuator1 = new Gpio(leds[1].gpio, 'out');
+actuator2 = new Gpio(leds[2].gpio, 'out');
+
+ function stop() {
+ LEDGpioConnection.write(leds[1]);
+ LEDGpioConnection.write(leds[2])
+ }
